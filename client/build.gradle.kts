4c43f00da54fa5914db4dbdfb5040dc839ef8a3a
Markéta Galdová <marketa@galdova.cz>
2024-03-02 14:12:59 +0100

    Build fat jar and shebangified jar

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -18,3 +18,29 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.mordant)
 }
+
+
+val fatJar = tasks.register<Jar>("fatJar") {
+    dependsOn("compileJava", "compileKotlin", "processResources")
+
+    manifest.attributes("Main-Class" to application.mainClass)
+    archiveBaseName.set("logfeline")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } + sourceSets.main.get().output)
+}
+
+val shebangifyJar = tasks.register("shebangifyJar") {
+    dependsOn(fatJar)
+    val sourceJar = fatJar.get().outputs.files.single()
+    val outputFile = layout.buildDirectory.file("bin/logfeline")
+    outputs.file(outputFile)
+    doFirst {
+        outputFile.get().asFile.outputStream().use { output ->
+            output.write("#!/bin/env -S java -jar\n".toByteArray())
+            sourceJar.inputStream().use { it.copyTo(output) }
+        }
+        outputFile.get().asFile.setExecutable(true, false)
+    }
+}
+
+tasks.getByName("build").dependsOn(fatJar, shebangifyJar)
